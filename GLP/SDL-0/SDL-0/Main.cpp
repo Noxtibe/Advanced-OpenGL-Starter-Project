2fa33170cb324831ebc1634931618ca1677675aa
Benoit <98055777+Noxtibe@users.noreply.github.com>
2023-04-18 11:31:26 +0200

    Pussh triangle

--- GLP/SDL-0/SDL-0/Main.cpp
+++ GLP/SDL-0/SDL-0/Main.cpp
@@ -2,7 +2,7 @@
 #include <SDL.h>
 #include <glew.h>
 using namespace std;
-//#define GLEW_STATIC
+#define GLEW_STATIC
 
 int main(int argc, char* argv[])
 {
@@ -17,7 +17,7 @@ int main(int argc, char* argv[])
 	///////////SETTING UP SDL/////////////
 	//Create a simple window
 	int width = 400;
-	int height = 300;
+	int height = 400;
 	unsigned int center = SDL_WINDOWPOS_CENTERED;
 	SDL_Window* Window = SDL_CreateWindow("My window", center, center, width, height, SDL_WINDOW_OPENGL);
 	//SDL_WINDOW_OPENGL is a u32 flag !
@@ -32,6 +32,117 @@ int main(int argc, char* argv[])
 	if (glewInit() == GLEW_OK) {
 		cout << "Glew initialized successfully\n";
 	}
+
+	//Describe the shape by its vertices
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	//Create an ID to be given at object generation
+	unsigned int vbo;
+
+
+	//Pass how many buffers should be created and the reference of the ID to get the value set
+	glGenBuffers(1, &vbo);
+
+
+	//Set the viewing frame through which we will see the objects
+	glViewport(0, 0, width, height);
+
+	//Put the color you want here for the background
+	glClearColor(0.0f, 0.5f, 0.2f, 1.0f);
+
+	const char* vertexShaderSource = "#version 330 core\n"
+		"in vec3 pos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(pos, 1.0);\n"
+		"}\0";
+
+
+	const char* fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\n\0";
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+
+	//now that we have a vertex shader, let’s put the code text inside
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+
+	//aaaaand… Compile !
+	glCompileShader(vertexShader);
+
+	//Do the same with the fragment shader !
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	//now attach shaders to use to the program
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	//and link it 
+	glLinkProgram(shaderProgram);
+
+	//now that the program is complete, we can use it 
+	glUseProgram(shaderProgram);
+
+	//Create one ID to be given at object generation
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+
+	//Binds the buffer linked to this ID to the vertex array buffer to be rendered. Put 0 instead of vbo to reset the value.
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	//Finally send the vertices array in the array buffer 
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	//Enable my vertex attrib array number 0 (we only have one attribute of position)
+	glEnableVertexAttribArray(0);
+
+	bool isRunning = true;
+	while (isRunning) {
+		// Inputs
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_QUIT:
+				isRunning = false;
+				break;
+			default:
+				break;
+			}
+		}
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the screen
+		//Shader to use next
+		glUseProgram(shaderProgram);
+
+		//VAO to use next
+		glBindVertexArray(vao);
+
+		//OMG WE FINALLY DRAW ! We use the GL_TRIANGLES primitive type
+		//We draw from vertex 0 and we will be drawing 3 vertices
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		SDL_GL_SwapWindow(Window); // Swapbuffer
+	}
+	// Quit
+	SDL_DestroyWindow(Window);
+	SDL_GL_DeleteContext(Context);
+
 
 	cin.get();
 	return 0;
